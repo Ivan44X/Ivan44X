b27a32ae00e3588c59e5e461a11d6ecf774b9b1b
Vanka <ivanix44@gmail.com>
2023-12-07 00:24:43 +1000

    чекай бота

---
+++ test.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('6000875693:AAHL366EdX5eJWplZS4s9333LusFyUyonHs')
+
+
+@bot.message_handler(commands=['start'])
+def markup (message):
+    markup = types.ReplyKeyboardMarkup ()
+    btn1 = types.KeyboardButton('проверка1')
+    btn2 = types.KeyboardButton('проверка2')
+    btn3 = types.KeyboardButton('проверка3')
+    markup.row(btn1,btn2,btn3)
+    markup.row(btn1,btn2,btn3)
+    file = open('./cyberforum_logo.png', 'rb')
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+    #bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
+   
+def on_click (message):
+    if message.text.lower() == 'проверка1':
+        bot.send_message(message.chat.id, 'проверка1 пройдена!')
+    elif message.text == 'проверка2':
+        bot.send_message(message.chat.id, 'проверка2 пройдена!')
+
+bot.polling(non_stop=True)
